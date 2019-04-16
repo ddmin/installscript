7b4cc88de2a5bdf33007c0ba879999de77270cf4
Daniel Min <danieldmin216@gmail.com>
2019-04-16 16:43:15 -0400

    initial commit

---
+++ install.sh
@@ -0,0 +1,12 @@
+# Install Sublime Text
+# .bashrc
+# .vimrc
+# Install neofetch, ranger, ueberzug, asciiquarium, cmatrix, tty-clock, mpsyt
+# Install virtualbox
+# Move playlists
+# Install fortune && fortunes
+# Install pip3 and pip
+# papirus icons
+# panel settings
+# install gimp
+# fzf
