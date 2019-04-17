5289a335cfb479ee3ebbc02b83531ee73e9d41ff
Daniel Min <danieldmin216@gmail.com>
2019-04-17 00:07:06 -0400

    added downloading programs

--- install.sh
+++ install.sh
@@ -1,15 +1,51 @@
 # Install Sublime Text
+sudo apt-get install apt-transport-https
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
 
 # install vim
+sudo apt install vim
 
 # .bashrc
+
 # .vimrc
-# Install neofetch, ranger, ueberzug, asciiquarium, cmatrix, tty-clock, mpsyt
+
+# Install neofetch
+sudo apt install neofetch
+
+# ranger
+
+# ueberzug
+
+# asciiquarium
+
+# cmatrix
+sudo apt install cmatrix
+
+# tty-clock
+
+# mpsyt
+
 # Install virtualbox
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+sudo apt update
+sudo apt install virtualbox-qt
+
 # Move playlists
+
 # Install fortune && fortunes
+
 # Install pip3 and pip
+sudo apt install python3-pip
+sudo apt install python-pip
+
 # papirus icons
+
 # panel settings
+
 # install gimp
+
 # fzf
