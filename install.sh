3b24c1400d23353c4094b5c8df0212f00228e871
Daniel Min <danieldmin216@gmail.com>
2019-04-21 00:37:17 -0400

    fixed to apt-get

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-# Update apt
-sudo apt -y update
-
 # Install git
-sudo apt -y install git
+sudo apt-get -y install git
+
+# panel settings
+mv panelsettings ~/panelsettings
 
 # Install fzf
 # NOTE: This must be manually installed
@@ -10,27 +10,27 @@ git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
 # Install pip3 and pip
-sudo apt -y install python3-pip
-sudo apt -y install python-pip
+sudo apt-get -y install python3-pip
+sudo apt-get -y install python-pip
 
 # Install gimp
-sudo apt -y install gimp
+sudo apt-get -y install gimp
 
 # Install neofetch
-sudo apt -y install neofetch
+sudo apt-get -y install neofetch
 
 # Install virtualbox
-sudo apt -y install virtualbox-qt
+sudo apt-get -y install virtualbox-qt
 
 # Install vim
-sudo apt -y install vim
+sudo apt-get -y install vim
 
 # Install Sublime Text
-sudo apt-y -get install apt-transport-https
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-y -key add -
+sudo apt-get -y install apt-transport-https
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-sudo apt-y -get update
-sudo apt-y -get install sublime-text
+sudo apt-get -y update
+sudo apt-get -y install sublime-text
 
 # ranger
 git clone https://github.com/ranger/ranger.git /tmp/ranger
@@ -41,9 +41,9 @@ cd ~
 # ueberzug
 
 # First install dependencies
-sudo apt -y install libjpeg-dev
-sudo apt -y install zlib1g-dev
-sudo apt -y install libxtst-dev
+sudo apt-get -y install libjpeg-dev
+sudo apt-get -y install zlib1g-dev
+sudo apt-get -y install libxtst-dev
 
 # Then install ueberzug
 sudo pip3 install ueberzug
@@ -56,16 +56,13 @@ mv /tmp/dot/.bashrc ~/.bashrc
 mv /tmp/dot/.bash_aliases ~/.bash_aliases
 
 # .vimrc
-mv .vimrc ~/.vimrc
+mv /tmp/dot/.vimrc ~/.vimrc
 
 # .gitconfig
-mv .gitconfig ~/.gitconfig
+mv /tmp/dot/.gitconfig ~/.gitconfig
 
 # ranger configs
-mv commands.py ~/.config/ranger
-mv rc.conf ~/.config/ranger
-mv rifle.conf ~/.config/ranger
-mv scope.sh ~/.config/ranger
-
-# panel settings
-mv panelsettings ~/panelsettings
+mv /tmp/dot/commands.py ~/.config/ranger
+mv /tmp/dot/rc.conf ~/.config/ranger
+mv /tmp/dot/rifle.conf ~/.config/ranger
+mv /tmp/dot/scope.sh ~/.config/ranger
