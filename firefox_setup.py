4222a8ec9ff6262ff080e4da781af7d1d6bb187c
Daniel Min <danieldmin216@gmail.com>
2019-09-29 23:46:40 -0400

    firefox addons take two

---
+++ firefox_setup.py
@@ -0,0 +1,29 @@
+# Open addons in Firefox to download
+import webbrowser
+
+def open_webpages():
+    SITES = []
+
+    # Homepage extension
+    SITES.append('https://addons.mozilla.org/en-US/firefox/addon/my-homepage/')
+
+    # Adblock
+    SITES.append('https://addons.mozilla.org/en-US/firefox/addon/adblock-plus/')
+
+    # Video Speed Controller
+    SITES.append('https://addons.mozilla.org/en-US/firefox/addon/videospeed/')
+
+    # Control + Number
+    SITES.append('https://addons.mozilla.org/en-US/firefox/addon/ctrl-number-to-switch-tabs/')
+
+    # SauceCodePro NerdFont
+    SITES.append('https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/SourceCodePro/Semibold/complete/Sauce%20Code%20Pro%20Semibold%20Nerd%20Font%20Complete%20Mono.ttf')
+
+    for site in SITES:
+        webbrowser.open(site)
+
+def main():
+    open_webpages()
+
+if __name__ == '__main__':
+    main()
