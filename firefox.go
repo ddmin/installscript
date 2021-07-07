7a56cb1d0d2e44ecfe604efd6f2240a65a8276b7
ddmin <ddmin216@outlook.com>
2021-07-07 19:33:38 -0400

    🙂 revamp

--- firefox.go
+++ firefox.go
@@ -16,39 +16,36 @@ type link struct {
 func index(w http.ResponseWriter, r *http.Request) {
 	SOFTWARE := []link{
 		{"BalenaEtcher", "https://www.balena.io/etcher/"},
-		{"Brave", "https://brave.com/linux/"},
 		{"CAVA", "https://github.com/karlstav/cava"},
 		{"GIMP Resynthesizer", "https://github.com/bootchk/resynthesizer"},
 		{"Gogh", "https://github.com/Mayccoll/Gogh"},
-		{"OBS", "https://obsproject.com/download"},
 		{"ProtonVPN", "https://protonvpn.com/download"},
 		{"RetroArch", "https://github.com/libretro/RetroArch"},
 		{"SauceCode Pro", "https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/SourceCodePro/Semibold/complete/Sauce%20Code%20Pro%20Semibold%20Nerd%20Font%20Complete%20Mono.ttf"},
 		{"Shotcut", "https://shotcut.org/download/"},
-		{"Tampermonkey", "https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/"},
 		{"VirtualBox", "https://www.virtualbox.org/wiki/Linux_Downloads"},
 		{"Webcamoid", "http://webcamoid.github.io/"},
 		{"youtube-dl", "https://youtube-dl.org/"},
 	}
 
 	FIREFOX := []link{
+		{"Crono", "https://addons.mozilla.org/en-US/firefox/addon/crono-app/"},
 		{"Ctrl+Number", "https://addons.mozilla.org/en-US/firefox/addon/ctrl-number-to-switch-tabs/"},
 		{"Custom New Tabs", "https://addons.mozilla.org/en-US/firefox/addon/custom-new-tabs/"},
 		{"Duplicate Tab", "https://addons.mozilla.org/en-US/firefox/addon/duplicate-tab-shortcut/"},
-		{"Stylus", "https://addons.mozilla.org/en-US/firefox/addon/styl-us/"},
 		{"Stylus Themes", "https://github.com/StylishThemes"},
-		{"uBlock Origin", "https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/"},
+		{"Stylus", "https://addons.mozilla.org/en-US/firefox/addon/styl-us/"},
+		{"Tampermonkey", "https://addons.mozilla.org/en-US/firefox/addon/tampermonkey/"},
 		{"Video Download Helper", "https://addons.mozilla.org/en-US/firefox/addon/video-downloadhelper/"},
 		{"Video Speed", "https://addons.mozilla.org/en-US/firefox/addon/videospeed/"},
+		{"uBlock Origin", "https://addons.mozilla.org/en-US/firefox/addon/ublock-origin/"},
 	}
 
 	CHROME := []link{
 		{"Bypass Paywalls", "https://github.com/iamadamdev/bypass-paywalls-chrome"},
-		{"Crono", "https://chrome.google.com/webstore/detail/crono/ffednpkacgekbgkcffkpoinbikhijadl?hl=en"},
 		{"Enable Right Click", "https://chrome.google.com/webstore/detail/enable-right-click/hhojmcideegachlhfgfdhailpfhgknjm?hl=en"},
 		{"LastPass", "https://chrome.google.com/webstore/detail/lastpass-free-password-ma/hdokiejnpimakedhajhdlcegeplioahd?hl=en-US"},
 		{"Stylus", "https://chrome.google.com/webstore/detail/stylus/clngdbkpkpeebahjckkjfobafhncgmne?hl=en"},
-		{"Video Speed Controller", "https://chrome.google.com/webstore/detail/video-speed-controller/nffaoalbilbmmfgbnbgppjihopabppdk?hl=en"},
 	}
 
 	SOURCES := []string{"Software", "Firefox", "Chrome"}
