072d0b6ae1ac3ebf8cbef1905851f7263449a40b
ddmin <danieldmin216@gmail.com>
2021-10-27 09:12:55 -0400

    🖱️ allow right click

--- firefox.go
+++ firefox.go
@@ -42,7 +42,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 	CHROME := []link{
 		{"Bypass Paywalls", "https://github.com/iamadamdev/bypass-paywalls-chrome"},
-		{"Enable Right Click", "https://chrome.google.com/webstore/detail/enable-right-click/hhojmcideegachlhfgfdhailpfhgknjm?hl=en"},
+		{"Allow Right-Click", "https://chrome.google.com/webstore/detail/allow-right-click/hompjdfbfmmmgflfjdlnkohcplmboaeo?hl=en"},
 		{"LastPass", "https://chrome.google.com/webstore/detail/lastpass-free-password-ma/hdokiejnpimakedhajhdlcegeplioahd?hl=en-US"},
 		{"Stylus", "https://chrome.google.com/webstore/detail/stylus/clngdbkpkpeebahjckkjfobafhncgmne?hl=en"},
 		{"Tabliss", "https://chrome.google.com/webstore/detail/tabliss-a-beautiful-new-t/hipekcciheckooncpjeljhnekcoolahp"},
